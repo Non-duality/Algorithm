f6eaa0ff0304bfcd06e020ac4cdb7a08b79acd0b
Jun <64851709+Non-duality@users.noreply.github.com>
2024-03-28 15:07:02 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,5 +1,11 @@
--- 코드를 입력하세요
+# -- 코드를 입력하세요
+# SELECT outs.ANIMAL_ID, outs.NAME
+# FROM ANIMAL_INS as ins right join ANIMAL_OUTS as outs
+# ON ins.ANIMAL_ID = outs.ANIMAL_ID
+# WHERE ins.ANIMAL_ID is null
 SELECT outs.ANIMAL_ID, outs.NAME
-FROM ANIMAL_INS as ins right join ANIMAL_OUTS as outs
-ON ins.ANIMAL_ID = outs.ANIMAL_ID
-WHERE ins.ANIMAL_ID is null
+FROM ANIMAL_OUTS as outs
+WHERE NOT EXISTS (
+    SELECT 1 FROM ANIMAL_INS as ins
+    WHERE ins.ANIMAL_ID = outs.ANIMAL_ID
+)
