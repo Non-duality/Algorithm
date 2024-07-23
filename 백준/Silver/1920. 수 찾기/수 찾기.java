50818e6d0c92e5d0129b08d30d2d3497ee071253
Jun <64851709+Non-duality@users.noreply.github.com>
2024-07-23 14:39:01 +0900

    [Silver IV] Title: 수 찾기, Time: 1756 ms, Memory: 62332 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int N, M;
+    private static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        arr = new int[N];
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < M; i++){
+            int result = binarySearch(Integer.parseInt(st.nextToken()));
+            System.out.println(result);
+        }
+
+    }
+    private static int binarySearch(int target){
+        int result = 0;
+
+        int start = 0;
+        int end = N-1;
+        int mid;
+        while(start <= end) {
+            mid =  (start + end) / 2;
+
+            if(target < arr[mid]){
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else if (target == arr[mid]) {
+                result = 1;
+                break;
+            }
+
+        }
+
+        return result;
+    }
+}
