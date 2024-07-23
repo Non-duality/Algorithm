b844e3de758b66cf9d9122b3c27e4af04b145886
Jun <64851709+Non-duality@users.noreply.github.com>
2024-07-23 16:13:13 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1064 ms, Memory: 180376 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -2,15 +2,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
 
     private static int N, M;
     private static int[] arr;
-    private static Map<Integer, Integer> map;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,49 +18,53 @@ public class Main {
         st = new StringTokenizer(br.readLine());
 
         arr = new int[N];
-        map = new HashMap<>();
         for(int i = 0; i < N; i++){
-            int number = Integer.parseInt(st.nextToken());
-            if(map.containsKey(number)){
-                int cnt = map.get(number);
-                cnt ++;
-                map.put(number, cnt);
-            }else {
-                map.put(number, 1);
-            }
-            arr[i] = number;
+            arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
 
         M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < M; i++){
-            int result = binarySearch(Integer.parseInt(st.nextToken()));
-            sb.append(result).append(" ");
+            int target = Integer.parseInt(st.nextToken());
+            sb.append(getUpperIndex(target) - getLowerIndex(target)).append(" ");
         }
         System.out.println(sb.toString());
     }
 
-    private static int binarySearch(int target){
-        int result = 0;
-
+    private static int getLowerIndex(int target){
         int start = 0;
-        int end = N-1;
+        int end = N;
         int mid;
 
-        while(start <= end){
+        while (start < end){
             mid = (start + end) / 2;
 
-            if(target < arr[mid]){
-                end = mid - 1;
-            } else if(target > arr[mid]) {
+            if(target <= arr[mid]){
+                end = mid;
+            }else{
                 start = mid + 1;
-            } else if(target == arr[mid]){
-                result = map.get(target);
-                break;
             }
         }
 
-        return result;
+        return start;
+    }
+
+    private static int getUpperIndex(int target){
+        int start = 0;
+        int end = N;
+        int mid;
+
+        while (start < end) {
+            mid = (start + end) / 2;
+
+            if(target < arr[mid]){
+                end = mid;
+            }else {
+                start = mid + 1;
+            }
+        }
+
+        return start;
     }
 }
