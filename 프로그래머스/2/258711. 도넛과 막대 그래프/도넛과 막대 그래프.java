e7909375289975e6605d86db3439d95ed254677b
Jun <64851709+Non-duality@users.noreply.github.com>
2024-08-27 00:53:50 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 373.44 ms, Memory: 392 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -1,40 +1,44 @@
 import java.util.*;
-
+    
 class Solution {
     public int[] solution(int[][] edges) {
+        int[] answer = new int[4];
+        ArrayList<Integer>[] outList = new ArrayList[1000001];
+        ArrayList<Integer>[] inList = new ArrayList[1000001];
         
-        Map<Integer, int[]> nodes = new HashMap<>();
-        int extraNode = -1;
-        int doughnut = 0;
-        int stick = 0;
-        int eight = 0;
-        
-        int from, to;
-        for(int[] edge : edges){
+        for(int i = 0; i < edges.length; i++) {
+            int a = edges[i][0];
+            int b = edges[i][1];
             
-            from = edge[0];
-            to = edge[1];
-            
-            if(!nodes.containsKey(from)) nodes.put(from, new int[] {0, 0});
-            if(!nodes.containsKey(to)) nodes.put(to, new int[] {0, 0});
-            
-            nodes.get(from)[0] ++;
-            nodes.get(to)[1] ++;
-            
+            if(outList[a] == null) outList[a] = new ArrayList<>();
+            if(outList[b] == null) outList[b] = new ArrayList<>();
+            if(inList[a] == null) inList[a] = new ArrayList<>();
+            if(inList[b] == null) inList[b] = new ArrayList<>();
+
+            outList[a].add(b);
+            inList[b].add(a);
         }
         
-        for(int key : nodes.keySet()){
-            
-            int[] count = nodes.get(key);
-            
-            if(count[0] >= 2 && count[1] == 0) extraNode = key;
-            else if(count[0] == 0) stick++;
-            else if(count[0] >= 2 && count[1] >=2) eight++;
-            
+        int total = 0;
+
+        for(int i = 1; i < 1000001; i++) {
+            if(outList[i] == null || inList[i] == null) continue;
+
+            if(outList[i].size() >= 2 && inList[i].size() == 0) {
+                total = outList[i].size();
+                answer[0] = i;
+            }
         }
+
+        for(int i = 1; i < 1000001; i++) {
+            if(outList[i] == null || inList[i] == null) continue;
+            if(i == answer[0]) continue;
+            if(outList[i].size() == 2) answer[3]++;
+            else if (outList[i].isEmpty()) answer[2]++;
+        }
+
+        answer[1] = total - (answer[2] + answer[3]);
         
-        doughnut = nodes.get(extraNode)[0] - (stick + eight);    
-        int[] answer = {extraNode, doughnut, stick, eight};
         return answer;
     }
 }
