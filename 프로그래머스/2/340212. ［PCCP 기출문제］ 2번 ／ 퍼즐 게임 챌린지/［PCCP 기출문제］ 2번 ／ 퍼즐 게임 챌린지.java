5ad29ff8a315005ba29a2b89bac29ae9799d2287
Jun <64851709+Non-duality@users.noreply.github.com>
2024-09-06 17:56:04 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 40.83 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,41 +1,46 @@
+import java.util.*;
+
 class Solution {
-    public int solution(int[] diffs, int[] times, long limit) {
-        int answer = 0;
+    public long solution(int[] diffs, int[] times, long limit) {
+        long answer = 0;
         
-        int start = 1;
-        int end = 100001;
-        while(start <= end){
-            int mid = (start + end) / 2;
-            long time = getLevel(diffs, times, mid);
-            if(time <= limit){
-                end = mid - 1;
-            }else{
-                start = mid + 1;
-            }
+        int length = diffs.length;
+        
+        long maxD = 1;
+        for(int i = 1; i < length; i++) {
+            maxD = Math.max(maxD, diffs[i]);
         }
-        answer = start;
-        return answer;
-    }
-    
-    public long getLevel(int[] diffs, int[] times, int tempLevel){
         
-        long totalTime = 0;
-        long timePrev = 0;
-        for(int i = 0; i < diffs.length; i++){
-            long diff = diffs[i];
-            long time = times[i];
+        long left = 1;
+        long right = maxD;
+        long level = (left + right) / 2;
+        
+        while(left <= right) {
+            long time = times[0];
             
-            if(diff > tempLevel){
-                totalTime += (time + timePrev) * (diff - tempLevel) + time;
-                timePrev = time;
-                continue;
+            for(int i = 1; i < length; i++) {
+                if(diffs[i] > level) {
+                    int t = times[i] + times[i - 1];
+                    
+                    long totalT = t * (diffs[i] - level) + times[i];
+                    
+                    time += totalT;
+                } else {
+                    time += times[i];
+                }
             }
-            else{
-                totalTime += time;
-                timePrev = time;
+            
+            if(time > limit) {
+                left = level + 1;
+                level = (left + right) / 2;
+            } else {
+                right = level - 1;
+                level = (left + right) / 2;
             }
         }
         
-        return totalTime;
+        answer = left;
+        
+        return answer;
     }
 }
