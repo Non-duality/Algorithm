cd1a3e71430da81793d5017e387e5bd2926e7f12
Jun <64851709+Non-duality@users.noreply.github.com>
2025-02-03 16:56:05 +0900

    [Bronze I] Title: 단어 공부, Time: 152 ms, Memory: 20584 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,42 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String temp = br.readLine();
-		temp = temp.toUpperCase();
-		
-		int[] arr = new int[91];
-		for(int i = 0; i < temp.length(); i++) {
-			arr[temp.charAt(i)] += 1;
-		}
-		int max = -1;
-		int cnt = 0;
-		int idx = -1;
-		for(int i = 65; i < 91; i++) {
-			if(max < arr[i]) {
-				max = arr[i];
-				idx = i;
-			}
-		}
-		
-		char result = (char)idx;
-		for(int i = 65; i < 91; i++) {
-			if(i == idx) {
-				continue;
-			}
-			if(max == arr[i]) {
-				result = '?';
-				break;
-			}
-		}
-		
-		System.out.println(result);
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
 
+        int[] count = new int[26];
+        for(int i = 0; i < str.length(); i++){
+            char temp = str.charAt(i);
+
+            // 소문자
+            if((int)temp >= (int)'a'){
+                count[temp - 'a'] += 1;
+            }
+            // 대문자
+            else{
+                count[temp - 'A'] += 1;
+            }
+        }
+
+        int max = -1;
+        char alpha = '?';
+        for(int i = 0; i < 26; i++){
+            if(max < count[i]){
+                max = count[i];
+                alpha = (char)(i + 'A');
+            }
+            else if(max == count[i]){
+                alpha = '?';
+            }
+        }
+        System.out.println(alpha);
+    }
 }
